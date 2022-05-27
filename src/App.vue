d838e47152d0384d63df2403fdeae670da4c179f
Dayana Ismailova <dayana530807@gmail.com>
2022-05-27 18:40:36 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Cover from "./layout/Cover.vue";
+</script>
+
+<template>
+  <Cover />
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+
+.container {
+  max-width: 1140px;
+  padding: 0 15px;
+  margin: 0 auto;
+}
+
+.btn {
+  border: none;
+  outline: none;
+  padding: 1rem 1.2rem;
+
+  font-size: 1rem;
+  font-weight: 600;
+  color: #fff;
+  background-color: transparent;
+
+  &--second{
+    border: 2px solid #fff;
+    border-radius: 10px;
+  }
+}
+
+img{
+  max-width: 100%;
+  height: auto;
+}
+</style>
---
+++ src/layout/Cover.vue
@@ -0,0 +1,70 @@
+<script setup></script>
+
+<template>
+  <div class="cover">
+    <div class="container">
+      <header class="cover__header">
+        <div class="logo-wrapper">
+          <img
+            class="logo-wrapper__img"
+            src="src/assets/logo white.png"
+            alt="logo-white"
+          />
+        </div>
+        <button class="btn btn--second">Заказать онлайн</button>
+      </header>
+      <h1 class="cover__title">
+        Сочный Арбуз прямо с&nbsp;грядки у&nbsp;фермера
+      </h1>
+      <div class="img-composition">
+        <div class="cloud-image">
+          <img src="src/assets/cloud1.png" alt="cloud1">
+        </div>
+        <div class="watermelon-image">
+          <img src="src/assets/watermelon с долками 1.png" alt="watermelon">
+        </div>
+        <div class="cloud-image">
+          <img src="src/assets/cloud2.png" alt="cloud2">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cover {
+  background-image: url("src/assets/background.jpg");
+  background-repeat: no-repeat;
+  background-position: 50% 20%;
+  background-size: cover;
+  height: 90vh;
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 20px;
+  }
+
+  &__title {
+    margin-top: 40px;
+    width: 80%;
+    font-size: 4rem;
+    color: #184604;
+  }
+}
+
+.img-composition{
+  
+}
+
+.cloud-image{}
+
+.watermelon-image{}
+
+.logo-wrapper {
+  &__img {
+    
+  }
+}
+</style>
